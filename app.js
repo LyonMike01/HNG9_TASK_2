03ee67596069bf7868684fa76ab6f1293c3e1c2c
Titiloye Temitope Michael <titimichael01@email.com>
2022-11-04 17:08:58 +0100

    Updated Operator

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
+const express = require("express");
 
-require('dotenv').config();
-const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(express.json());
-const cors = require("cors");
-app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 const slackUsername = "TitiMike"
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
-const operationTypes = ["addition", "subtraction", "multiplication", "add", "sub", "mul"];
-
-app.post('/', (req, res) => {
-    let x = req.body.x;
-    let y = req.body.y;
-    let operation = req.body.operation_type.toLowerCase();
-
-    if(isNaN(x) || isNaN(y)){
-        res.status(400).send("Numbers required for x and y fields");
-    } else if(!operationTypes.includes(operation)){
-        res.status(400).send("Please select a valid operation");
-    } else {
-        let result
-        switch(operation){
-            case "addition":
-                result = x + y;
-                break;
-            case "subtraction":
-                result = x - y;
-                break;
-            case "multiplication":
-                result = x * y;
-                break;
-            case "add":
-                result = x + y;
-                break;
-            case "subt":
-                result = x - y;
-                break;
-            case "mul":
-                result = x * y;
-                break;
-            default:
-                result = null; 
-        }
-        const myResponse = {
-            slackUsername: slackUsername,
-            result: result,
-            operation_type: req.body.operation_type
-        }
-        res.status(201).json(myResponse);
+app.post("/", (req, res) => {
+  let result = "0";
+  let operation_type;
+  let x;
+  let y;
+  const { operation_type: op, x: nx, y: ny } = req.body;
+  if (op.split(" ").length > 1) {
+    if (op.includes("add")) {
+      operation_type = "addition";
+    } else if (op.includes("sub") || op.includes("minus")) {
+      operation_type = "subtraction";
+    } else if (op.includes("multiply") || op.includes("times")) {
+      operation_type = "multiplication";
     }
-})
+    let arr = op.split(" ");
+    arr = arr.filter((i) => !isNaN(parseInt(i)));
+    if (arr.length === 2) {
+      console.log(arr);
+      x = arr[0];
+      y = arr[1];
+    }
+  } else {
+    operation_type = op;
+    x = nx;
+    y = ny;
+  }
 
-
-app.listen(PORT, () =>{
-    console.log(`App is running on port ${PORT}`)
-  })
+  x = parseInt(x);
+  y = parseInt(y);
+  switch (operation_type) {
+    case "addition":
+        result = x + y;
+        break;
+    case "subtraction":
+        result = x - y;
+        break;
+    case "multiplication":
+        result = x * y;
+        break;
+    case "add":
+        result = x + y;
+        break;
+    case "subt":
+        result = x - y;
+        break;
+    case "mul":
+        result = x * y;
+        break;
+  }
+  return res
+    .status(200)
+    .json({ slackUsername: slackUsername, result, operation_type });
+});
+app.listen(PORT || 8000, () => console.log(`server running on ${PORT}`));
